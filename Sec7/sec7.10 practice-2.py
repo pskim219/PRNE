d3e111a3d9e78cd310ba851783e9f613767c4d34
pskim219 <pskim219@yahoo.com>
2019-07-10 15:12:32 -0500

    create new py file for sec 7.10

--- Sec7/sec7.10 practice-2.py
+++ Sec7/sec7.10 practice-2.py
@@ -1,12 +1,27 @@
-# Create a LIST of DICTIONARIES to hold device inforemation about multiple device
-
+# Create a LIST of DICTIONARIES to hold device information about multiple device
 from pprint import pprint
+devices = []
 
 file = open('devices','r')
-#file_line = file.readline().strip()
-# Open the file and read in the information one line at a time.
-# Put the information for each device into a list.
-for line in file:  # file is
-    device_info_list = line.strip().split()
+for line in file:
+    device_info_list = line.strip().split(',') # Get device info into list
+
+    device_info = {} # Create the inner dictionary of device info
+    device_info['name'] = device_info_list[0]
+    device_info['os-type'] = device_info_list[1]
+    device_info['ip'] = device_info_list[2]
+    device_info['username'] = device_info_list[3]
+    device_info['password'] = device_info_list[4]
+
+print 'device_info:',device_info
 print
-print device_info_list
+pprint(device_info)
+
+devices.append(device_info)
+print
+print
+
+pprint(devices)
+
+file.close()
+
